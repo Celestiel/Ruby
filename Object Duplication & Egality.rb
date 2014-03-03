8db1030631405e4b0e207065a2d11942ec492873
Celestiel <Celestiel@live.be>
2014-03-03 20:00:46 +0100

    Update Object Duplication & Egality.rb

--- Object Duplication & Egality.rb
+++ Object Duplication & Egality.rb
@@ -1,11 +1,12 @@
 #==============================================================================
 # ** Object
 #------------------------------------------------------------------------------
-# Let's play with Object.
+# Utility: Recursive version of duplication and equality 
 #==============================================================================
-class Object
+class Object # /!\ This Script is a Nuke
   #--------------------------------------------------------------------------
-  # object.is_duplicable?
+  # Some object respond to dup and clone but raise an error when the fonction 
+  # is being call 
   #--------------------------------------------------------------------------
   def is_duplicable?
     !self.class.ancestors.any? do |k|
@@ -13,18 +14,18 @@ class Object
     end
   end
   #--------------------------------------------------------------------------
-  # Recursive Fonction : Duplicate Object and SubObject
+  # - Recursive Fonction: inst_dup => mimic => inst_dup
+  # * Duplicate an object and set set all the instances of duplicated_object 
+  # as deplicated version of original object's instance_variable.
   #--------------------------------------------------------------------------
   def inst_dup
-    return self if !self.is_duplicable? 
-    duplicated_object = self.dup
-    self.instance_variables.all? do |instance|
-      duplicated_object.instance_variable_set(instance,self.instance_variable_get(instance).inst_dup)
-    end
+    return self if !self.is_duplicable?
+    duplicated_object = self.dup.mimic(self)
     return duplicated_object
   end
   #--------------------------------------------------------------------------
-  # mimic instance of 
+  # * duplicate all instance_variable of arg and set them as instance_variable 
+  # of self
   #--------------------------------------------------------------------------
   def mimic(object)
     object.instance_variables.all? do |instance|
@@ -32,7 +33,9 @@ class Object
     end
   end
   #--------------------------------------------------------------------------
-  # * Object Equality
+  # - Recursive Fonction: inst_eql? => inst_eql? 
+  # * Egality test between two object by recusion. Two object are equal if 
+  # all the instance variable are equal.
   #--------------------------------------------------------------------------
   def inst_eql?(object)
     if self.is_a?(object.class)
@@ -42,11 +45,10 @@ class Object
     end
   end
 end
-
 #==============================================================================
 # ** Array
 #------------------------------------------------------------------------------
-# 
+# - Array is not coded in RUBY and don't use instance_variable
 #==============================================================================
 class Array
   def inst_dup # Special case of inst_dup
@@ -56,21 +58,32 @@ class Array
   end
 end
 #==============================================================================
-# ** Hash 
+# ** Hash
 #------------------------------------------------------------------------------
-# 
+# - Hash is not coded in RUBY and don't use instance_variable
 #==============================================================================
 class Hash
-  def inst_dup #  Special case of inst_dup
+  def inst_dup 
     duplicated_object = self.dup
     self.each{|key,instance| duplicated_object[key] = instance.inst_dup}
     return duplicated_object
   end
 end
 #==============================================================================
+# ** Proc
+#------------------------------------------------------------------------------
+# - Proc is not coded in RUBY and don't use instance_variable
+# dup/clone work on Proc but it's quite useless...
+#==============================================================================
+class Proc 
+  def inst_dup
+    return self 
+  end
+end
+#==============================================================================
 # ** Module Eql
 #------------------------------------------------------------------------------
-# 
+# Include this module in a class to set == and eql? method as inst_eql? 
 #==============================================================================
 module Eql
   def eql?(object); self.inst_eql?(object);end
